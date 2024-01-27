115f39a731500ea2c6bb4172286b42bf5e8a7010
Sainath <71361447+sainath161@users.noreply.github.com>
2024-01-27 19:27:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import './style.css'
-import React,{useState} from 'react';
-import Books from './components/Books';
-import BookInfo from './components/BookInfo';
-function App() {
-  const[bookinfo,setBookInfo]=useState([])
+import React, { useState, useEffect } from "react";
+import NavBar from "./Components/NavBar";
+import Books from "./Components/Books";
+import MoreBooks from "./Components/MoreBooks";
+import BookDetails from "./Components/BookDetails";
+import axios from "axios";
+
+const App = () => {
+  const [books, setBooks] = useState(undefined);
+  const [query, setQuery] = useState(null);
+
+  const [bookClicked, setBookClicked] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=${
+          query ? query : "HarryPotter"
+        }&maxResults=30`
+      )
+      .then((res) => setBooks(res.data.items))
+      .catch((err) => console.log(err));
+    console.log(books);
+  }, [query]);
+
   return (
     <div>
-       <BookInfo bookinfo={bookinfo}/>
-      <Books setBookInfo={setBookInfo}  bookinfo={bookinfo}/>
-     
+      <NavBar setQuery={setQuery} setBookClicked={setBookClicked} />
+      {console.log(bookClicked)}
+      {bookClicked ? (
+        <BookDetails bookClicked={bookClicked} />
+      ) : books != undefined ? (
+        <Books booksDetail={books} setBookClicked={setBookClicked} />
+      ) : (
+        ""
+      )}
+
+      {books != undefined ? (
+        <MoreBooks booksDetail={books} setBookClicked={setBookClicked} />
+      ) : (
+        ""
+      )}
     </div>
   );
-}
+};
 
 export default App;
